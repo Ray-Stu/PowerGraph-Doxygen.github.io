795dcf2b2319993beac563119abb27f3201d379b
Ray-Stu <Ray_Stu@163.com>
2020-02-26 16:46:51 +0800

    commit

---
+++ navtreedata.js
@@ -0,0 +1,105 @@
+var NAVTREE =
+[
+  [ "GraphLab: Distributed Graph-Parallel API", "index.html", [
+    [ "FAQ", "FAQ.html", null ],
+    [ "Basic GraphLab Tutorial", "using_graphlab.html", "using_graphlab" ],
+    [ "GraphLab Warp System Tutorial", "using_warp.html", "using_warp" ],
+    [ "Graph File Formats", "graph_formats.html", [
+      [ "Portable Formats", "graph_formats.html#graph_portable_formats", [
+        [ "tsv (edge list)", "graph_formats.html#graph_tsv_format", null ],
+        [ "snap (edge list)", "graph_formats.html#graph_snap_format", null ],
+        [ "adj (adjacency list)", "graph_formats.html#graph_adj_format", null ],
+        [ "bintsv4 (binary edge list)", "graph_formats.html#graph_bintsv4_format", null ]
+      ] ],
+      [ "Non-Portable Formats", "graph_formats.html#graph_nonportable_formats", [
+        [ "graphjrl (Graph Journal)", "graph_formats.html#graph_format_graphjrl", null ],
+        [ "bin (Distributed Graph Binary)", "graph_formats.html#graph_format_bin", null ]
+      ] ]
+    ] ],
+    [ "GraphLab RPC", "RPC.html", [
+      [ "Examples", "RPC.html#sec_examples", null ],
+      [ "Spawning and Initialization", "RPC.html#sec_spawning", [
+        [ "Spawning with MPI", "RPC.html#sec_spawning_mpi", null ],
+        [ "Spawning with rpcexec.py", "RPC.html#sec_spawning_rpcexec", null ]
+      ] ],
+      [ "RPC Usage Overview", "RPC.html#sec_rpc_usage", [
+        [ "Basic RPC", "RPC.html#sec_basic_rpc_usage", null ],
+        [ "Collective Operations", "RPC.html#sec_rpc_collective", [
+          [ "Barrier", "RPC.html#sec_rpc_collective_barrier", null ],
+          [ "Full Barrier", "RPC.html#sec_rpc_collective_fullbarrier", null ],
+          [ "Other Collectives", "RPC.html#sec_rpc_collective_other_collectives", null ]
+        ] ],
+        [ "Sequentialization", "RPC.html#sec_rpc_sequentialization", null ]
+      ] ],
+      [ "Distributed Objects", "RPC.html#OOP_RPC", [
+        [ "Usage", "RPC.html#sec_oop_rpc_usage", null ],
+        [ "Context", "RPC.html#sec_oop_rpc_context", null ],
+        [ "Final Notes", "RPC.html#sec_oop_rpc_notes", null ]
+      ] ],
+      [ "Fiber Compatible Remote Requests", "RPC.html#Fiber_RPC", null ]
+    ] ],
+    [ "Serialization", "serialization.html", [
+      [ "Basic serialize/deserialize", "serialization.html#sec_basic_serialize", null ],
+      [ "Serializable", "serialization.html#sec_serializable", [
+        [ "Integer Types", "serialization.html#sec_serializable_integers", null ],
+        [ "Floating Point Types", "serialization.html#sec_serializable_floats", null ],
+        [ "Containers", "serialization.html#sec_serializable_containers", null ]
+      ] ],
+      [ "User Structs and Classes", "serialization.html#sec_serializable_user", null ],
+      [ "POD Serialization", "serialization.html#sec_serializable_pod", null ],
+      [ "Out of Place Serialization", "serialization.html#sec_serializable_out_of_place", null ]
+    ] ],
+    [ "GraphLab Toolkits", "toolkits.html", "toolkits" ],
+    [ "模块", "modules.html", "modules" ],
+    [ "命名空间", null, [
+      [ "命名空间列表", "namespaces.html", "namespaces" ],
+      [ "命名空间成员", "namespacemembers.html", [
+        [ "全部", "namespacemembers.html", null ],
+        [ "函数", "namespacemembers_func.html", null ],
+        [ "变量", "namespacemembers_vars.html", null ],
+        [ "类型定义", "namespacemembers_type.html", null ],
+        [ "枚举", "namespacemembers_enum.html", null ],
+        [ "枚举值", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "类", "annotated.html", [
+      [ "类列表", "annotated.html", "annotated_dup" ],
+      [ "类索引", "classes.html", null ],
+      [ "类继承关系", "hierarchy.html", "hierarchy" ],
+      [ "类成员", "functions.html", [
+        [ "全部", "functions.html", "functions_dup" ],
+        [ "函数", "functions_func.html", "functions_func" ],
+        [ "变量", "functions_vars.html", null ],
+        [ "类型定义", "functions_type.html", null ],
+        [ "枚举", "functions_enum.html", null ],
+        [ "枚举值", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "文件", null, [
+      [ "文件列表", "files.html", "files" ],
+      [ "文件成员", "globals.html", [
+        [ "全部", "globals.html", null ],
+        [ "函数", "globals_func.html", null ],
+        [ "宏定义", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classgraphlab_1_1blocking__queue.html#abccdc834aafa82a464a3659fb7f5acb4",
+"classgraphlab_1_1dense__bitset.html#a1978342938ea0102ac13429ea62cf9a7",
+"classgraphlab_1_1distributed__oblivious__ingress.html#a278f2d375bfcf5c1e93692f1b597c2f3",
+"classgraphlab_1_1hopscotch__map.html#a6123a37b571059399e8f913627565dca",
+"classgraphlab_1_1mutable__queue.html#a948d93c05c138a0c2d3ac414532676b5",
+"classgraphlab_1_1safe__circular__char__buffer.html#afdfe236fb00b217a9d245c56f551dbc0",
+"dc__buffered__stream__send2_8cpp_source.html",
+"group__random.html#ga9a7e6e3eae225419ae07a6c55e53f5f3",
+"structgraphlab_1_1cache__line__pad.html#a5fc45bf91adfdd8d26ece91f9cfd13ff",
+"zookeeper__common_8cpp_source.html"
+];
+
+var SYNCONMSG = '点击 关闭 面板同步';
+var SYNCOFFMSG = '点击 开启 面板同步';
---
+++ search/searchdata.js
@@ -0,0 +1,48 @@
+var indexSectionsWithContent =
+{
+  0: "12345678_abcdefghijklmnopqrstuvwxy~",
+  1: "abcdefghijklmopqrstuvw",
+  2: "bgt",
+  3: "gjlo",
+  4: "_abcdefghijklmnoprstuvwxy~",
+  5: "_aceghijlmnprsuv",
+  6: "acdeghilmprsuv",
+  7: "des",
+  8: "aefinorst",
+  9: "clo",
+  10: "gmrsw",
+  11: "12345678bcfglpst"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "typedefs",
+  7: "enums",
+  8: "enumvalues",
+  9: "defines",
+  10: "groups",
+  11: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "全部",
+  1: "类",
+  2: "命名空间",
+  3: "文件",
+  4: "函数",
+  5: "变量",
+  6: "类型定义",
+  7: "枚举",
+  8: "枚举值",
+  9: "宏定义",
+  10: "组",
+  11: "页"
+};
+
